292d09dc1d053cf29ca66cc987dbf700e795c7aa
robertogamez <vn00m8demail.wal/mart.com>
2018-12-13 09:23:32 -0600

    Proyecto de ejemplo para capitulo de formularios

---
+++ forms-example/src/basic-controls.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Frm(){
+    return <div>
+        <input type="text" value="react" />
+        <br />
+        <textarea value="react"></textarea>
+        <br />
+        <select value="sunday">
+            <option value="saturday">Saturday</option>
+            <option value="sunday">Sunday</option>
+        </select>
+    </div>
+}
+
+export default Frm;
+
