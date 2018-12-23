92904e0394f3731e0a71fa71a2ee51436dea040b
robertogamez <roberto.mb.gamez@hotmail.com>
2018-12-22 22:46:15 -0600

    Trabajando con react-redux

--- starting-redux/src/index.js
+++ starting-redux/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Redux from 'redux';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
@@ -9,19 +10,34 @@ let model = {
     time: 0
 };
 
-let view = (model) => {
-    let minutes = Math.floor(model.time / 60);
-    let seconds = model.time - (minutes * 60);
-    let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`;
-    let handler = (event) => {
-        container.dispatch(model.running ? { type: intents.STOP } : { type: intents.START });
-    }
-
-    return <div>
-        <p>{minutes}:{secondsFormatted}</p>
-        <button onClick={handler}>{model.running ? 'Stop' : 'Start'}</button>
-    </div>
+function mapStateToProps(state) {
+    return state;
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onStart: () => {
+            dispatch({type: 'START'});
+        },
+        onStop: () => {
+            dispatch({type: 'STOP'});
+        }
+    };
+}
+
+let StopWatch = ReactRedux
+    .connect(mapStateToProps, mapDispatchToProps)((props) => {
+        let minutes = Math.floor(props.time / 60);
+        let seconds = props.time - (minutes * 60);
+        let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`;
+
+        return <div>
+            <p>{minutes}:{secondsFormatted}</p>
+            <button onClick={props.running ? props.onStop : props.onStart}>
+                {props.running ? 'Stop' : 'Start'}
+            </button>
+        </div>
+    });
 
 let intents = {
     TICK: 'TICK',
@@ -32,11 +48,11 @@ let intents = {
 
 const update = (model = { running: false, time: 0 }, action) => {
     const updates = {
-        'TICK': (model) => Object.assign(model, {
+        'TICK': (model) => Object.assign({}, model, {
             time: model.time + (model.running ? 1 : 0)
         }),
-        'STOP': (model) => Object.assign(model, {running: false}),
-        'START': (model) => Object.assign(model, {running: true})
+        'STOP': (model) => Object.assign({}, model, { running: false }),
+        'START': (model) => Object.assign({}, model, { running: true })
     };
 
     return (updates[action.type] || (() => model))(model);
@@ -44,11 +60,11 @@ const update = (model = { running: false, time: 0 }, action) => {
 
 let container = Redux.createStore(update);
 
-const render = () => {
-    ReactDOM.render(view(container.getState()), document.getElementById('root'));
-}
-
-container.subscribe(render);
+ReactDOM.render(
+    <ReactRedux.Provider store={container}>
+        <StopWatch />
+    </ReactRedux.Provider>,
+    document.getElementById('root'));
 
 setInterval(() => {
     container.dispatch({ type: intents.TICK });
